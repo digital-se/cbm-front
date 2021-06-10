97966e6fceb3a94b9a53ddf0fbbc1318c1d97e15
Natalie Macedo <macedonatalie@gmail.com>
2021-06-10 12:11:44 -0300

    Refatoração da página de inicio

--- src/components/Inicio/Inicio.js
+++ src/components/Inicio/Inicio.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { withTranslation, Trans } from 'react-i18next';
 import ContentWrapper from '../Layout/ContentWrapper';
-import { Container, Row, Col, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
+import { Container, Row, Col, Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Card, CardBody, ButtonGroup, CardHeader } from 'reactstrap';
 import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
 import { Button, Form, FormGroup, Label, FormText } from 'reactstrap';
 import { Link } from 'react-router-dom';
+
 
 import qs from "qs";
 
@@ -13,7 +14,11 @@ import ResultCard from "../Comp/ResultCard"
 import db from "../../db/db"
 
 class SearchResult extends React.Component {
-
+    constructor(props) {
+        super(props);
+        this.state = {height: props.height};
+      }
+     
     state = {
         dropdownOpen: false,
         resultados: []
@@ -41,53 +46,58 @@ class SearchResult extends React.Component {
     componentDidMount() {
         this.getSearch()
     }
+    UNSAFE_componentWillMount(){ //pegar o tamanho da pagina pra centralizar 
+        this.setState({height: window.outerHeight*0.575 + 'px'});
+      }
 
     render() {
         return (
-            <ContentWrapper>
-                <div className="content-heading">
-                   <div>
-                    <h2>Sistema Interno de Gestão Documental</h2>
-                    </div>   
-                </div>
-                <div>
-                   <p>  </p>
-                </div>
-                <Row style={{ justifyContent: 'center', alignItems: 'center' }}>
-                    <Col xl={3} md={6}>
-                        <Row style={{ justifyContent: 'center', alignItems: 'center'}}>
-                            <Link to={"/busca"} style={{ color: '#fff',textDecoration: 'none'}}> 
-                                <div style={{ "width": "250px"}} className="card flex-row align-items-center align-items-stretch border-0">
-                                    <div style = {{ backgroundColor: '#fff'}} className="btn-outline-light"></div>
-                                    <div style={{ backgroundColor: '#d7130f' }} className="col-3 d-flex align-items-center justify-content-center rounded-left">
-                                        <em className="fa icon-magnifier fa-3x" />
+        <ContentWrapper>
+            <div className="d-flex align-items-center justify-content-center container container-table" style={{"height": this.state.height}}>
+                <Row style={{ justifyContent: 'center', alignItems: 'center',display:'flex'}} class="col-xs-4 col-xs-offset-4">
+                    <Card style={ {width: "400px" }} class="shadow-lg p-3 mb-5 bg-white rounded">
+                        <CardHeader><center><h1>Home</h1></center></CardHeader>
+                            <CardBody>
+                                <Col style={{ justifyContent: 'center', alignItems: 'center'}}>
+                                    <div className="text-center">
+                                        <Row style={{ justifyContent: 'center', 
+                                                    alignItems: 'center'}}>
+                                            <Link to={"/busca"} style={{ color: '#fff',textDecoration: 'none'}}> 
+                                                <div style={{ "width": "250px","height": "100px"}} className="card flex-row align-items-center align-items-stretch border-0 rounded-3">
+                                                    <div style = {{ backgroundColor: '#fff'}} className="btn-outline-light"></div>
+                                                    <div style={{ backgroundColor: '#d7130f' }} className="col-3 d-flex align-items-center justify-content-center rounded-left">
+                                                        <em className="fa icon-magnifier fa-3x" />
+                                                    </div>
+                                                    <div style={{ backgroundColor: '#f13430' }} className="col-9 py-3 d-flex align-items-center justify-content-center rounded-right btn-outline-light" >
+                                                        <div className="h2  mt-0">Buscar documentos</div>
+                                                        <div className="text-uppercase">{this.data}</div>
+                                                    </div>                   
+                                                </div>                                  
+                                            </Link>                                                                
+                                        </Row>                                         
                                     </div>
-                                    <div style={{ backgroundColor: '#f13430' }} className="col-9 py-3 d-flex align-items-center justify-content-center rounded-right btn-outline-light" >
-                                        <div className="h2  mt-0">Busca</div>
-                                        <div className="text-uppercase">{this.data}</div>
-                                    </div>                   
-                                </div>   
-                            </Link>             
-                        </Row>    
-                    </Col>
-                    
-                    <Col xl={3} md={6} className="text-center " > 
-                        <Row style={{ justifyContent: 'center', alignItems: 'center'}}>
-                            <Link to={"/cadastro"} style={{ color: '#fff',textDecoration: 'none' }}>
-                                <div style={{ "width": "250px" }} className="card flex-row align-items-center align-items-stretch border-0">
-                                    <div style={{ backgroundColor: '#d7130f' }} className="col-3 d-flex align-items-center justify-content-center rounded-left" >
-                                        <em className="fa fa-file fa-3x" />
+
+                                    <div className="text-center "> 
+                                        <Row style={{ justifyContent: 'center', alignItems: 'center'}}>
+                                            <Link to={"/cadastro"} style={{ color: '#fff',textDecoration: 'none' }}>
+                                                <div style={{ "width": "250px" }} className="card flex-row align-items-center align-items-stretch border-0">
+                                                    <div style={{ backgroundColor: '#d7130f' }} className="col-3 d-flex align-items-center justify-content-center rounded-left" >
+                                                        <em className="fa fa-file fa-3x" />
+                                                    </div>
+                                                    <div style={{ backgroundColor: '#f13430' }} className="col-9 py-3 d-flex align-items-center justify-content-center rounded-right btn-outline-light" >
+                                                        <div className="h2  mt-0">Cadastrar documentos</div>
+                                                        <div className="text-uppercase">{this.data}</div>
+                                                    </div>
+                                                </div>
+                                            </Link>
+                                        </Row>        
                                     </div>
-                                    <div style={{ backgroundColor: '#f13430' }} className="col-9 py-3 d-flex align-items-center justify-content-center rounded-right btn-outline-light" >
-                                        <div className="h2  mt-0">Cadastro</div>
-                                        <div className="text-uppercase">{this.data}</div>
-                                    </div>
-                                </div>
-                            </Link>
-                        </Row>        
-                    </Col>
+                                </Col>  
+                        </CardBody>      
+                    </Card>
                 </Row>
-            </ContentWrapper>
+            </div>
+        </ContentWrapper>
         );
     }
 }
