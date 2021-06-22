4442370188037b01b273ac91557460a7e17f129b
João Pedro Nimuendaju <joao.nimuendaju@souunit.com.br>
2021-06-22 14:13:13 -0300

    correção de formatação

--- src/components/Inicio/Inicio.js
+++ src/components/Inicio/Inicio.js
@@ -16,9 +16,9 @@ import db from "../../db/db"
 class SearchResult extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {height: props.height};
-      }
-     
+        this.state = { height: props.height };
+    }
+
     state = {
         dropdownOpen: false,
         resultados: []
@@ -46,50 +46,52 @@ class SearchResult extends React.Component {
     componentDidMount() {
         this.getSearch()
     }
-    
-    UNSAFE_componentWillMount(){
-        this.setState({height: window.outerHeight*0.575 + 'px'}); //remover, manter 620 como padrão é melhor :s
-      }
+
+    UNSAFE_componentWillMount() {
+        this.setState({ height: window.outerHeight * 0.575 + 'px' }); //remover, manter 620 como padrão é melhor :s
+    }
 
 
     render() {
         return (
-        
-        
-        <ContentWrapper>
-        
-            <div className="d-flex align-items-center justify-content-center container container-table" style={{"height": "630px"}}>
-                <Row style={{ justifyContent: 'center', alignItems: 'center',display:'flex'}} class="col-xs-4 col-xs-offset-4">
-                    <Card style={ {"width": "520px", "height": "570px", borderRadius: '20px', "box-shadow": "#ccc"}} class="shadow-lg p-3 mb-5 bg-white rounded">
 
-                        <CardHeader><center><h1 style={{"font-size":"4.5rem"}}>Home</h1></center></CardHeader>
+
+            <ContentWrapper>
+
+                <div className="d-flex align-items-center justify-content-center container container-table" style={{ "height": "630px" }}>
+                    <Row style={{ justifyContent: 'center', alignItems: 'center', display: 'flex' }} class="col-xs-4 col-xs-offset-4">
+                        <Card style={{ "width": "520px", "height": "570px", borderRadius: '20px', "box-shadow": "#ccc" }} class="shadow-lg p-3 mb-5 bg-white rounded">
+
+                            <CardHeader><center><h2 style={{ "font-size": "4.5rem" }}>Home</h2></center></CardHeader>
                             <CardBody>
-                                <div style={{"height" : "40px"}}></div>
-                                <Col style={{ justifyContent: 'center', alignItems: 'center'}}>
+                                <div style={{ "height": "40px" }}></div>
+                                <Col style={{ justifyContent: 'center', alignItems: 'center' }}>
                                     <div className="text-center">
-                                        <Row style={{ justifyContent: 'center', 
-                                                    alignItems: 'center'}}>
-                                            <Link to={"/busca"} style={{ color: '#fff',textDecoration: 'none'}}> 
-                                            {/*atualizar para nav e aumentar uns 25% de tudo ai*/}
-                                                <div style={{ "width": "350px","height": "130px", borderRadius: '20px'}} className="card flex-row align-items-center align-items-stretch border-0 rounded">
-                                                    <div style = {{ backgroundColor: '#fff'}} className="btn-outline-light"></div>
+                                        <Row style={{
+                                            justifyContent: 'center',
+                                            alignItems: 'center'
+                                        }}>
+                                            <Link to={"/busca"} style={{ color: '#fff', textDecoration: 'none' }}>
+                                                {/*atualizar para nav e aumentar uns 25% de tudo ai*/}
+                                                <div style={{ "width": "350px", "height": "130px", borderRadius: '20px' }} className="card flex-row align-items-center align-items-stretch border-0 rounded">
+                                                    <div style={{ backgroundColor: '#fff' }} className="btn-outline-light"></div>
                                                     <div style={{ backgroundColor: '#d7130f' }} className="col-3 d-flex align-items-center justify-content-center rounded-left">
                                                         <em className="fa icon-magnifier fa-3x" />
                                                     </div>
                                                     <div style={{ backgroundColor: '#f13430' }} className="col-9 py-3 d-flex align-items-center justify-content-center rounded-right btn-outline-light" >
                                                         <div className="h2  mt-0">Buscar documentos</div>
                                                         <div className="text-uppercase">{this.data}</div>
-                                                    </div>                   
-                                                </div>                                  
-                                            </Link>                                                                
-                                        </Row>                                         
+                                                    </div>
+                                                </div>
+                                            </Link>
+                                        </Row>
                                     </div>
-                                    <div style={{"height" : "20px"}}></div>
+                                    <div style={{ "height": "20px" }}></div>
 
-                                    <div className="text-center "> 
-                                        <Row style={{ justifyContent: 'center', alignItems: 'center'}}>
-                                            <Link to={"/cadastro"} style={{ color: '#fff',textDecoration: 'none' }}>
-                                                <div style={{ "width": "350px","height": "130px" }} className="card flex-row align-items-center align-items-stretch border-0">
+                                    <div className="text-center ">
+                                        <Row style={{ justifyContent: 'center', alignItems: 'center' }}>
+                                            <Link to={"/cadastro"} style={{ color: '#fff', textDecoration: 'none' }}>
+                                                <div style={{ "width": "350px", "height": "130px" }} className="card flex-row align-items-center align-items-stretch border-0">
                                                     <div style={{ backgroundColor: '#d7130f' }} className="col-3 d-flex align-items-center justify-content-center rounded-left" >
                                                         <em className="fa fa-file fa-3x" />
                                                     </div>
@@ -99,14 +101,14 @@ class SearchResult extends React.Component {
                                                     </div>
                                                 </div>
                                             </Link>
-                                        </Row>        
+                                        </Row>
                                     </div>
-                                </Col>  
-                        </CardBody>      
-                    </Card>
-                </Row>
-            </div>
-        </ContentWrapper>
+                                </Col>
+                            </CardBody>
+                        </Card>
+                    </Row>
+                </div>
+            </ContentWrapper>
         );
     }
 }
