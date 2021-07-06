f6a20c1ffdfa5dc2cd13caea5d8f2b173934be9b
Natalie Macedo <macedonatalie@gmail.com>
2021-07-06 15:06:30 -0300

    Update
    Co-Authored-By: João Pedro Nimuendaju  <63682822+Pareiollya@users.noreply.github.com>

--- src/components/Comp/ResultCard.js
+++ src/components/Comp/ResultCard.js
@@ -6,99 +6,58 @@ import { Button, Form, FormGroup, Label, FormText } from 'reactstrap';
 import { InputGroup, InputGroupAddon, InputGroupText, Input } from 'reactstrap';
 
 class ResultCard extends Component {
-
+        
     constructor(props) {
         super(props)
-        // titulo pode ser edição ou nome completo ¯\_(ツ)_/¯
         this.nome = this.props.nome
         this.data = this.props.data
         this.tipo = this.props.tipo
         this.numeracao = this.props.numeracao
         this.militares = this.props.militares
+        this.descricao = this.props.descricao
 
+        
+        
         if (this.tipo === "diario") {
             this.url = this.props.url
+            this.color = "#7266ba"
+
+
+        } else if (this.tipo === "BGA"){
+            this.color = "#2f80e7"
+    
+        }else if (this.tipo === "BGO"){
+            this.color = "#7266ba"
+
+        }else if (this.tipo === "ficha"){
+            this.color = "#D04545"
+            this.url = "/ficha/" + this.numeracao
         }
+        
     }
 
     render() {
-        if (this.tipo === "ficha") {
+        if (this.tipo === "ficha" || this.tipo === "diario") {
             return (
-                <Row style={{ justifyContent: "center", alignItems: "center" }}>
-                    <Card style={{ width: "16rem", justifyContent: "center" }}>
-                        <Row>
-                            <Col>
-                                <a
-                                    href={"/ficha/" + this.num}
-                                    style={{
-                                        width: "16rem",
-                                        justifyContent: "center",
-                                        color: "#fff",
-                                        cursor: "pointer",
-                                    }}
-                                >
-                                    <CardHeader style={{ width: "16rem", alignItems: "center", justifyContent: "center", backgroundColor: "#9F75FF" }} >
-                                        <Row>
-                                            <div style={{ backgroundColor: "#9F75FF", width: "3rem", justifyContent: "center", alignItems: "right" }}>
-                                                <em className="fa fa-address-card fa-3x"></em>
-                                            </div>
-                                            <div style={{ backgroundColor: "#95FF", width: "10rem", justifyContent: "center", alignItems: "left" }}>
-                                                <h5> Fichas de arroz</h5>
-                                            </div>
-                                        </Row>
-                                    </CardHeader>
-                                </a>
-                                <CardBody>
-                                    <Row>
-                                        <Col>
-                                            <Label>Tipo:</Label>
-                                            <div style={{ width: "15px" }} />
-                                            <Label>Data:</Label>
-                                            <div style={{ width: "15px" }} />
-                                            <Label>.... Militares adicionados</Label>
-
-                                        </Col>
-                                    </Row>
-                                    <Row>
-                                        <Col>
-                                            <FormGroup>
-                                                <Label for="descricao">Descrição</Label>
-                                                <Input
-                                                    disabled
-                                                    type="textarea"
-                                                    name="descricao"
-                                                    id="descricao"
-                                                    value={this.nome}
-                                                    style={{
-                                                        resize: "none",
-                                                        height: "50px",
-                                                        cursor: "pointer",
-                                                    }}
-                                                />
-                                            </FormGroup>
-                                        </Col>
-                                    </Row>
-                                </CardBody>
-                            </Col>
-                        </Row>
-                    </Card>
-                </Row>
-            );
-
-        } else if (this.tipo === "diario") {
-            return (
-
-                <a href={this.url} target="_blank" rel="noreferrer" style={{ textDecoration: 'none' }}>
+                <a 
+                href={this.url}
+                target="_blank" 
+                rel="noreferrer"
+                style={{
+                    width: "16rem",
+                    justifyContent: "center",
+                    color: "#FFFF",
+                    cursor: "pointer",
+                    textDecoration: 'none' 
+                }}>
                     <Card style={{ borderRadius: '10px', width: "16rem", justifyContent: "center" }}>
-                        <CardHeader style={{ backgroundColor: '#20c997', color: "#fff" }}>
+                        <CardHeader style={{ backgroundColor: this.color, color: "#FFFF", justifyContent: "center" }}>
                             <Row >
-                                <div className="col-3 d-flex align-items-center justify-content-center">
-                                    <em className="fas fa-newspaper fa-3x"></em>
+                                <div className="col-3 d-flex align-items-center justify-content: mr-4 ml-1 ">
+                                    <em className="fa fa-address-card fa-3x"></em>
                                 </div>
-
-                                <div className="col-6 py-3 rounded-right">
-                                    <div className="h5 mt-0">{this.nome} Diario de feijão</div>
-                                    {this.data} 99/99/999
+                                <div className="col-6 py-3 d-flex rounded-right">
+                                    <div className="h5">{this.nome} Fichas de arroz</div>
                                 </div>
                             </Row>
                         </CardHeader>
@@ -108,16 +67,20 @@ class ResultCard extends Component {
                                     <div style={{ width: "15px" }} />
                                     <Label>Tipo: {this.tipo}</Label>
                                     <div style={{ width: "15px" }} />
+                                    <Label>Data: {this.data} 21/10/2021</Label>
+                                    <div style={{ width: "15px" }} />
+                                    <div style={{ width: "15px" }} />
+                                    <Label>{this.militares} Militares adicionados</Label>
                                 </Col>
                             </Row>
                             <Label for="descricao">Descrição</Label>
                             <Input
                                 disabled
                                 type="textarea"
-                                value={this.tipo}
+                                value={this.descricao}
                                 style={{
                                     resize: "none",
-                                    height: "50px",
+                                    height: "75px",
                                     cursor: "pointer",
                                 }}
                             />
@@ -125,7 +88,52 @@ class ResultCard extends Component {
                     </Card>
                 </a >
             );
-        }
+
+        //esse card 
+        } {/* else if (this.tipo === "diario" || this.tipo === "BGA" || this.tipo === "BGO" || this.tipo === "BIR") {
+            return (
+                <a href={this.url} target="_blank" rel="noreferrer" style={{ textDecoration: 'none' }}>
+                    
+                <Card style={{ borderRadius: '10px', width: "16rem", justifyContent: "center" }}>
+                    <CardHeader className="col-12" style={{background: this.color, color: "#fff", height: "4rem"}}>
+                        <Row >
+                            <div className="col-3 d-flex align-items-center justify-content-center">
+                                <em className="fas fa-newspaper fa-3x"></em>
+                            </div>
+                            <div className="col-9 py-2" >
+                                <div className="h5 mt-0">{this.nome} Diario de feijão</div>
+                                
+                            </div>
+                        </Row>
+                    </CardHeader>
+
+                    <CardBody style={{ color: '#343a40' }}>
+                        <Row>
+                            <Col>
+                                <Label>Tipo: {this.tipo}</Label>
+                                <div style={{ width: "15px" }} />
+                                <Label>Data: {this.data} 21/10/2021</Label>
+                                <div style={{ width: "15px" }} />
+                                <Label>n Militares associados</Label>
+                            </Col>
+                        </Row>
+                        <Label for="descricao">Descrição</Label>
+                        <Input
+                            disabled
+                            type="textarea"
+                            value="disgraçaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa disgraçaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa disgraçaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+                            style={{
+                                resize: "none",
+                                height: "75px",
+                                cursor: "pointer",
+                            }}
+                        />
+                    </CardBody>
+                </Card>
+            </a >
+               
+            );
+        } */}
     }
 }
 
