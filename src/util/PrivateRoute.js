0fe62d42d4f08cf1600c6fd09db1abe25decb791
João Pedro Nimuendaju <joao.nimuendaju@souunit.com.br>
2021-07-28 14:39:27 -0300

    Keycloack component PrivateRoute #6

---
+++ src/util/PrivateRoute.js
@@ -0,0 +1,37 @@
+import { useKeycloak } from '@react-keycloak/web';
+import React from 'react';
+import { Redirect, Route } from 'react-router-dom';
+
+export function PrivateRoute({ component: Component, roles, ...rest }) {
+    const { keycloak, initialized } = useKeycloak();
+
+    const isAutherized = (roles) => {
+        if (keycloak && roles) {
+            return roles.some(r => {
+                // No KeyCloak tem 2 formas de colocar cargos em usuários 
+                // Você pode colocar cargos no Realm e no Client 
+                // Neste caso você pode usar ambos os cenários com os hasRealmRole & hasResourceRole
+                const realm = keycloak.hasRealmRole(r);
+                const resource = keycloak.hasResourceRole(r);
+                return realm || resource;
+            });
+        }
+        return false;
+    }
+
+    return (
+        <Route
+            {...rest}
+            render={props => {
+                return isAutherized(roles)
+                    ? <Component {...props} />
+                    : <Redirect
+                        to={{
+                            pathname: '/',
+                        }}
+                    />
+            }
+            }
+        />
+    )
+}
