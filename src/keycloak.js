e8d47dcd57fc51c67c53afb04460aca88c6dd32f
João Pedro Nimuendaju <joao.nimuendaju@souunit.com.br>
2021-08-04 10:42:37 -0300

    arar o solo

--- src/keycloak.js
+++ src/keycloak.js
@@ -4,5 +4,16 @@ const keycloakConfig = {
     realm: 'Testes',
     clientId: 'front-react'
 }
+{/*
+    const keycloakConfig = 
+    {
+    "realm": "master",
+    "auth-server-url": "https://sandbox-safepro.cbm.se.gov.br/auth/",
+    "ssl-required": "external",
+    "resource": "digital-se",
+    "public-client": true,
+    "confidential-port": 0
+    }
+ */}
 const keycloak = new Keycloak(keycloakConfig);
 export default keycloak
