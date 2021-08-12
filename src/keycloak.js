794024399d42dcaf4f6cf6670a8736e7f8919faf
João Pedro Nimuendaju <joao.nimuendaju@souunit.com.br>
2021-08-12 11:58:46 -0300

    migração keycloak API #70

--- src/keycloak.js
+++ src/keycloak.js
@@ -1,19 +1,14 @@
 import Keycloak from 'keycloak-js'
 const keycloakConfig = {
-    url: 'https://kc.piratasdoraio.com/auth',
-    realm: 'Testes',
-    clientId: 'front-react'
-}
-{/*
-    const keycloakConfig = 
-    {
+
     "realm": "master",
-    "auth-server-url": "https://sandbox-safepro.cbm.se.gov.br/auth/",
+    "url": "https://sandbox-safepro.cbm.se.gov.br/auth/",
     "ssl-required": "external",
     "resource": "digital-se",
     "public-client": true,
-    "confidential-port": 0
-    }
- */}
+    "confidential-port": 0,
+    "clientId": "digital-se",
+
+}
 const keycloak = new Keycloak(keycloakConfig);
 export default keycloak
