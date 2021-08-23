254edec0041ff2589a6b4d71afcec80fbc9e80ea
João Pedro Nimuendaju <joao.nimuendaju@souunit.com.br>
2021-08-23 14:48:47 -0300

    Ajuste para nova rota de edição de cadastro

--- src/components/Documento/Documento.js
+++ src/components/Documento/Documento.js
@@ -197,7 +197,7 @@ class Documento extends React.Component {
                                         <h3>Informações adicionais</h3>
                                     </CardHeader>
                                     <div style={{ "padding": "15px", "max-width": "200px" }}>
-                                        <Button href= {"/editar/" + this.state.documento.id}
+                                        <Button href= {"/documentos/" + this.state.documento.id + "/editar"}
                                             color="danger" 
                                             disabled={this.state.loading}> 
                                             <em className="fa mr-2 fas fa-pencil-alt "/>Editar Documento
