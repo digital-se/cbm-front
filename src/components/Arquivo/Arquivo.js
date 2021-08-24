e5643de41992583cfbf6015e9bfa96abefd51d11
João Pedro Nimuendaju <joao.nimuendaju@souunit.com.br>
2021-08-24 11:51:48 -0300

    Redirect caso acesse arquivo ou documento inexistente

--- src/components/Arquivo/Arquivo.js
+++ src/components/Arquivo/Arquivo.js
@@ -4,10 +4,11 @@ import { withTranslation } from 'react-i18next';
 import ContentWrapper from '../Layout/ContentWrapper';
 import { Row, Col } from 'reactstrap';
 import { Input } from 'reactstrap';
-import { Button} from 'reactstrap';
+import { Button } from 'reactstrap';
 import { Card, CardBody, CardHeader } from 'reactstrap';
 import { Carousel, CarouselItem } from 'reactstrap';
 import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
+import { Redirect } from 'react-router-dom';
 import Swal from '../Comp/Swal';
 import axios from "axios"
 
@@ -16,7 +17,7 @@ class Arquivo extends React.Component {
     state = {
         dropdownOpen: false,
         arquivo: {
-            id:"",
+            id: "",
             campos: {
                 nome: "Carregando...",
                 descricao: "Carregando...",
@@ -32,7 +33,8 @@ class Arquivo extends React.Component {
         editArq: {
             nome: "",
             descricao: "",
-        }
+        },
+        redirect: false,
     }
 
     toggle = () => {
@@ -86,139 +88,152 @@ class Arquivo extends React.Component {
     }
 
     async componentDidMount() {
+        try {
+            let arquivo = await axios.get(`https://sandbox-api.cbm.se.gov.br/api-digitalse/arquivo/${this.props.match.params.id}`)
 
-        let arquivo = await axios.get(`https://sandbox-api.cbm.se.gov.br/api-digitalse/arquivo/${this.props.match.params.id}`)
+            let arq = {
+                id: arquivo.id,
+                campos: {
+                    nome: arquivo.nome,
+                    descricao: arquivo.descricao,
+                },
+                arquivos: arquivo.arquivos.map((arq) => {
+                    return {
+                        src: `https://sandbox-api.cbm.se.gov.br/api-digitalse/arquivo/${this.props.match.params.id}/arquivos/${arq.id}/arquivo`,
+                        caption: arq.nome,
+                        ocr: arq.texto
+                    }
+                })
+            }
 
-        let arq = {
-            id:arquivo.id,
-            campos: {
-                nome: arquivo.nome,
-                descricao: arquivo.descricao,
-            },
-            arquivos: arquivo.arquivos.map((arq) => {
-                return {
-                    src: `https://sandbox-api.cbm.se.gov.br/api-digitalse/arquivo/${this.props.match.params.id}/arquivos/${arq.id}/arquivo`,
-                    caption: arq.nome,
-                    ocr: arq.texto
-                }
-            })
+            await this.setState({ arquivo: arq })
+            this.awaitResult(true)
+
+
+        } catch (e) {
+            this.setState({ redirect: true })
+
         }
 
-        await this.setState({ arquivo: arq })
-        this.awaitResult(true)
 
     }
 
     render() {
-        return (
-            <ContentWrapper>
-                <Row>
-                    <Col>
-                        <Card className="card-default" style={{ justifyContent: 'center' }}>
-                            <Row>
-                                <Col sm={12} lg={8}>
-                                    <CardHeader style={{ textAlign: 'center' }}>
-                                        <h3>{this.state.arquivo.campos.nome}</h3>
-                                    </CardHeader>
-                                    <CardBody style={{ "resize": "none" }}>
-                                        <h4 style={{ textAlign: "center", backgroundColor: "#ebebeb", borderRadius: "5px", margin: "16px", padding: "12px", fontWeight: "bold" }}>{this.state.arquivo.arquivos[this.state.carousel.activeIndex]?.caption}</h4>
-                                        <Carousel
-                                            activeIndex={this.state.carousel.activeIndex}
-                                            next={this.next}
-                                            previous={this.previous}
-                                            interval={false}
-                                            style={{ minHeight: "650px" }}
-                                        >
-                                            {this.state.arquivo.arquivos.map((item) => {
-                                                return (
-                                                    <CarouselItem
-                                                        onExiting={() => this.setState({ carousel: { ...this.state.carousel, animating: true } })}
-                                                        onExited={() => this.setState({ carousel: { ...this.state.carousel, animating: false } })}
-                                                        key={item.src}
-                                                        style={{ "padding": "0" }}
-                                                    >
-                                                        <Swal options={{
-                                                            imageUrl: item.src,
-                                                            width: "80%",
-                                                            imageAlt: 'Carregando...',
-                                                            showConfirmButton: false,
-                                                            showCloseButton: true
-                                                        }} className="btn d-md-block">
-                                                            <img src={item.src} alt={item.caption} style={{ alignmentAdjust:"auto", maxWidth: "100%", maxHeight: "550px" }} />
-                                                        </Swal>
-                                                    </CarouselItem>
-                                                );
-                                            })}
-                                        </Carousel>
-                                        <Pagination aria-label="Page navigation example" className="d-flex justify-content-center">
-                                            <PaginationItem>
-                                                <PaginationLink previous onClick={this.previous} />
-                                            </PaginationItem>
-                                            {this.state.arquivo.arquivos.map((item, index) => {
-                                                return (
-                                                    <PaginationItem key={index}>
-                                                        <PaginationLink onClick={() => this.goToIndex(index)}>
-                                                            {index + 1}
-                                                        </PaginationLink>
-                                                    </PaginationItem>
-                                                )
-                                            })}
-                                            <PaginationItem>
-                                                <PaginationLink next onClick={this.next} />
-                                            </PaginationItem>
-                                        </Pagination>
-                                    </CardBody>
-                                </Col>
-                                <Col sm={12} lg={4}>
-                                    <CardBody>
-                                        <Card>
-                                            <CardHeader>
-                                                <h3>Texto Extraido</h3>
-                                            </CardHeader>
-                                            <div style={{ "padding": "15px", "max-width": "200px" }}> 
-                                                <Button
-                                                    disabled={this.state.loading} 
-                                                    color="danger"> 
-                                                    <em className="fa mr-2 fas fa-pencil-alt "/>Editar Arquivo 
-                                                </Button >
-                                            </div>
-                                            <CardBody>
-                                                <Input disabled
-                                                    type="textarea"
-                                                    name="ocr"
-                                                    id="ocr"
-                                                    value={this.state.arquivo.arquivos[this.state.carousel.activeIndex]?.ocr}
-                                                    style={{ "resize": "none", "height": "390px" }} />
-                                              <Row>
-                                                  <div className="ml-auto mr-auto">
-                                                      <Button 
-                                                          hidden={this.state.editArquivo} 
-                                                          color="danger" 
-                                                          onClick={this.discardChangesArquivo}
-                                                          style={{ "height": "35px", "width":"99px" }}> 
-                                                          <em className="fa mr-2 fas fa-times" />Cancelar
-                                                      </Button>
-                                                      <Button 
-                                                          hidden={this.state.editArquivo} 
-                                                          color="success" 
-                                                          onClick={this.salvarArquivo}
-                                                          type="button"
-                                                          className="ml-4"
-                                                          style={{ "height": "35px", "width":"99px" }}> 
-                                                          Salvar<em className="fa ml-2 fas fa-check" />
-                                                      </Button>
-                                                  </div>
-                                              </Row>
-                                            </CardBody>
-                                        </Card>
-                                    </CardBody>
-                                </Col>
-                            </Row>
-                        </Card>
-                    </Col>
-                </Row>
-            </ContentWrapper >
-        );
+        if (this.state.redirect === true) {
+            return (
+                <Redirect to="/inicio" />
+            );
+        } else {
+            return (
+                <ContentWrapper>
+                    <Row>
+                        <Col>
+                            <Card className="card-default" style={{ justifyContent: 'center' }}>
+                                <Row>
+                                    <Col sm={12} lg={8}>
+                                        <CardHeader style={{ textAlign: 'center' }}>
+                                            <h3>{this.state.arquivo.campos.nome}</h3>
+                                        </CardHeader>
+                                        <CardBody style={{ "resize": "none" }}>
+                                            <h4 style={{ textAlign: "center", backgroundColor: "#ebebeb", borderRadius: "5px", margin: "16px", padding: "12px", fontWeight: "bold" }}>{this.state.arquivo.arquivos[this.state.carousel.activeIndex]?.caption}</h4>
+                                            <Carousel
+                                                activeIndex={this.state.carousel.activeIndex}
+                                                next={this.next}
+                                                previous={this.previous}
+                                                interval={false}
+                                                style={{ minHeight: "650px" }}
+                                            >
+                                                {this.state.arquivo.arquivos.map((item) => {
+                                                    return (
+                                                        <CarouselItem
+                                                            onExiting={() => this.setState({ carousel: { ...this.state.carousel, animating: true } })}
+                                                            onExited={() => this.setState({ carousel: { ...this.state.carousel, animating: false } })}
+                                                            key={item.src}
+                                                            style={{ "padding": "0" }}
+                                                        >
+                                                            <Swal options={{
+                                                                imageUrl: item.src,
+                                                                width: "80%",
+                                                                imageAlt: 'Carregando...',
+                                                                showConfirmButton: false,
+                                                                showCloseButton: true
+                                                            }} className="btn d-md-block">
+                                                                <img src={item.src} alt={item.caption} style={{ alignmentAdjust: "auto", maxWidth: "100%", maxHeight: "550px" }} />
+                                                            </Swal>
+                                                        </CarouselItem>
+                                                    );
+                                                })}
+                                            </Carousel>
+                                            <Pagination aria-label="Page navigation example" className="d-flex justify-content-center">
+                                                <PaginationItem>
+                                                    <PaginationLink previous onClick={this.previous} />
+                                                </PaginationItem>
+                                                {this.state.arquivo.arquivos.map((item, index) => {
+                                                    return (
+                                                        <PaginationItem key={index}>
+                                                            <PaginationLink onClick={() => this.goToIndex(index)}>
+                                                                {index + 1}
+                                                            </PaginationLink>
+                                                        </PaginationItem>
+                                                    )
+                                                })}
+                                                <PaginationItem>
+                                                    <PaginationLink next onClick={this.next} />
+                                                </PaginationItem>
+                                            </Pagination>
+                                        </CardBody>
+                                    </Col>
+                                    <Col sm={12} lg={4}>
+                                        <CardBody>
+                                            <Card>
+                                                <CardHeader>
+                                                    <h3>Texto Extraido</h3>
+                                                </CardHeader>
+                                                <div style={{ "padding": "15px", "max-width": "200px" }}>
+                                                    <Button
+                                                        disabled={this.state.loading}
+                                                        color="danger">
+                                                        <em className="fa mr-2 fas fa-pencil-alt " />Editar Arquivo
+                                                    </Button >
+                                                </div>
+                                                <CardBody>
+                                                    <Input disabled
+                                                        type="textarea"
+                                                        name="ocr"
+                                                        id="ocr"
+                                                        value={this.state.arquivo.arquivos[this.state.carousel.activeIndex]?.ocr}
+                                                        style={{ "resize": "none", "height": "390px" }} />
+                                                    <Row>
+                                                        <div className="ml-auto mr-auto">
+                                                            <Button
+                                                                hidden={this.state.editArquivo}
+                                                                color="danger"
+                                                                onClick={this.discardChangesArquivo}
+                                                                style={{ "height": "35px", "width": "99px" }}>
+                                                                <em className="fa mr-2 fas fa-times" />Cancelar
+                                                            </Button>
+                                                            <Button
+                                                                hidden={this.state.editArquivo}
+                                                                color="success"
+                                                                onClick={this.salvarArquivo}
+                                                                type="button"
+                                                                className="ml-4"
+                                                                style={{ "height": "35px", "width": "99px" }}>
+                                                                Salvar<em className="fa ml-2 fas fa-check" />
+                                                            </Button>
+                                                        </div>
+                                                    </Row>
+                                                </CardBody>
+                                            </Card>
+                                        </CardBody>
+                                    </Col>
+                                </Row>
+                            </Card>
+                        </Col>
+                    </Row>
+                </ContentWrapper >
+            );
+        }
     }
 }
 Arquivo.propTypes = {
