53cf7b8a80bccf56c3142dfa6e96a8bc87bbfa5e
Natalie Macedo <macedonatalie@gmail.com>
2021-08-30 21:03:33 -0300

    Retirada (finalmente) da sidebar

--- src/components/Layout/Base.js
+++ src/components/Layout/Base.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Header from './Header'
-import Sidebar from './Sidebar'
 
 const Base = props => (
     <div className="wrapper">
         <Header />
-
-        <Sidebar />
-
         <section className="section-container">
             {props.children}
         </section>
